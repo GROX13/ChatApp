341ba7f16a7d3e28e825183ee6a1c2bcf3a361a5
Giorgi <giorgirokhadze@gmail.com>
2015-05-21 01:39:26 +0400

    Update URLContactListDownloaderTask.java

--- app/src/main/java/org/giorgi/chatapp/asynchtasks/URLContactListDownloaderTask.java
+++ app/src/main/java/org/giorgi/chatapp/asynchtasks/URLContactListDownloaderTask.java
@@ -5,12 +5,23 @@ import org.giorgi.chatapp.model.Contact;
 import java.util.ArrayList;
 
 public class URLContactListDownloaderTask extends ContactListDownloaderTask {
+    private String url;
+
+    public URLContactListDownloaderTask(String url) {
+        this.url = url;
+    }
 
     @Override
     protected ArrayList<Contact> doInBackground(Void... params) {
         // TODO Auto-generated method stub
 
         return null;
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<Contact> contactList) {
+        // TODO Auto-generated method stub
+
     }
 
 }
